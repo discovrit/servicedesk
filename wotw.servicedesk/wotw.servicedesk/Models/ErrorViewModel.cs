5bde3310dfc40288bb6eb7d23e4b08f65d40ab7f
Luke Shaw <Luke@DESKTOP-SF0H1J2>
2020-05-19 13:40:34 +0100

    Initial

---
+++ wotw.servicedesk/wotw.servicedesk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wotw.servicedesk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
